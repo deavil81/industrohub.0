a22b86cb955436bc98c478675d501491f888d6be
deavil81 <109474534+deavil81@users.noreply.github.com>
2026-03-09 18:47:02 +0530

    Update scriptURL for RFQ form submission

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbz9VFwKdlJoX0KaQNQB4hbELExoaK2waTBYCgCY0eHqTf8sxkL9YkxGZnaLy6s7x34EXw/exec";
+const scriptURL =https://script.google.com/macros/s/AKfycbxuqarRFEjIWfQjWta68JXrJo9udH9SJAYbr-vDcX-TwhXAYafVSxhfZ1A6oH_msNFLuw/exec;
 
 document.getElementById("rfqForm").addEventListener("submit", function(e) {
 
@@ -21,3 +21,4 @@ document.getElementById("rfqForm").addEventListener("submit", function(e) {
   });
 
 });
+
