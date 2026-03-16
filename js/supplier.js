612137a53167290fca419f2a8459c17ebb962f49
deavil81 <109474534+deavil81@users.noreply.github.com>
2026-03-16 23:57:23 +0530

    Update supplier.js

--- js/supplier.js
+++ js/supplier.js
@@ -19,15 +19,20 @@ const product = cols[1] || "N/A";
 const category = cols[2] || "N/A";
 const quantity = cols[4] || "N/A";
 const location = cols[5] || "N/A";
+const date = cols[0];
 
 const card = document.createElement("div");
 card.className = "rfq-card";
 
 card.innerHTML = `
 <h3>${product}</h3>
+
+<p class="rfq-meta">📅 Posted: ${date}</p>
+
 <p><strong>Category:</strong> ${category}</p>
 <p><strong>Quantity:</strong> ${quantity}</p>
 <p><strong>Location:</strong> ${location}</p>
+
 <a class="btn-primary" href="rfq.html">Send Quote</a>
 `;
 
